41e85212e8b913f3ee551692d8256282b13fb137
Nabi Karampoor <thisisnabi@outlook.com>
2023-09-04 20:43:29 +0330

    feat: Add database context

--- src/Program.cs
+++ src/Program.cs
@@ -1,31 +1,26 @@
  
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddAuthentication("cookie")
+        .AddCookie("cookie", o =>
+        {
+            o.LoginPath = "/login";
+            o.LogoutPath = "/logout";
 
+        });
 }
 
-builder.Services.AddAuthentication("cookie")
-    .AddCookie("cookie", o => {
-        o.LoginPath = "/login";
-        o.LogoutPath = "/logout";
-
-    });
-
 builder.Services.AddAuthorization();
-
 
 var app = builder.Build();
 {
     app.UseHttpsRedirection();
-
     app.UseAuthorization();
-
 }
 
 app.MapGet("/login", Getlogin.Handler);
 app.MapPost("/login", Login.Handler);
 app.MapGet("/oauth/authorize", AuthorizationEndpoint.Handler).RequireAuthorization();
 app.MapPost("/oauth/token", TokenEndpoint.Handler);
-
 
 app.Run();
