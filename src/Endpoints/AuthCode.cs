43d3d9a25a17099f2a32728137a136a02c2d66a6
Nabi Karampoor <thisisnabi@outlook.com>
2023-09-15 15:59:05 +0330

    init: Initial project setup files

---
+++ src/Endpoints/AuthCode.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace Devblogs.IdentitiesProvider.Endpoints
+{
+    internal class AuthCode
+    {
+        public StringValues ClientId { get; set; }
+        public StringValues CodeChallenge { get; set; }
+        public StringValues CodeChallengeMethod { get; set; }
+        public StringValues RedirectUri { get; set; }
+        public DateTime Expiry { get; set; }
+    }
+}
